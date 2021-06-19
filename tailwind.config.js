ccf3171d556bcada22d3dfe0f969c06dac7ef4d4
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-06-19 19:59:04 +0530

    Update tailwind.config.js (#8)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,16 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],  
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      orange: '#F15A24',
-    },
+    gradientColorStops: theme => ({
+       ...theme('colors'),
+       'orange': '#F15A24',
+      })
+    extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      gradientColorStops: ['active'],
+    },
   },
   plugins: [],
 }

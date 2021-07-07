853d9ccc6d4e74a046bc13be000713d72ffa3a30
Akalanka Perera <akalankaperera128@gmail.com>
2021-07-07 13:00:12 +0530

    resolved conflicts

--- components/announcements/Announcements.tsx
+++ components/announcements/Announcements.tsx
@@ -11,14 +11,14 @@ export default function Announcements(props: AnnouncementProps): JSX.Element {
   return (
     <section className="flex flex-col md:flex-row justify-center items-center ">
       <div className="flex flex-col py-10 w-10/12 px-3">
-        <div className="flex flex-col md:flex-row pb-4 md:pb-0 items-center text-2xl font-semibold md:text-left text-center mb-10 bg-gray-lighter rounded-lg shadow-noOffset_lighter">
+        <div className="flex flex-col md:flex-row pb-4 md:pb-0 items-center text-2xl font-semibold md:text-left text-center mb-10 bg-gray-100 rounded-lg shadow-noOffset_lighter">
           <GiSpeaker size={70} className="mx-5" />
           <div>
             <span className="underline">{props.title.split(' ')[0]}</span>
             <span>{' ' + props.title.split(' ')[1]}</span>
           </div>
         </div>
-        <div className="bg-gray-lighter px-12 md:px-24 py-4 pt-7 rounded-lg shadow-noOffset_lighter">
+        <div className="bg-gray-100 px-12 md:px-24 py-4 pt-7 rounded-lg shadow-noOffset_lighter">
           <div className="w-full md:w-1/2">
             {props.announcments.map((announcement) => {
               return (
--- pages/notices.tsx
+++ pages/notices.tsx
@@ -92,7 +92,7 @@ export default function Notices(): JSX.Element {
         </div>
       </div>
       <div className="w-full md:w-11/12 flex flex-row justify-center md:justify-end">
-        <div className="bg-orange-dark mr-0 md:mr-4 px-5 py-3 rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 text-white font-semibold cursor-pointer transition ease-in duration-200 flex flex-row items-center">
+        <div className="bg-fcsc-orange mr-0 md:mr-4 px-5 py-3 rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 text-white font-semibold cursor-pointer transition ease-in duration-200 flex flex-row items-center">
           <MdPermContactCalendar size={28} className="mr-3" />
 
           <div>Notice Calendar</div>

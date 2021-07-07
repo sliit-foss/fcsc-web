9beb81c6ccbc5c1e58b12143b0a1fa16b925acc8
MadhushaPrasad <madushaprasad21@gmail.com>
2021-07-07 23:39:20 +0530

    create navbar desktop viewe

--- components/common/navbar/Navbar.tsx
+++ components/common/navbar/Navbar.tsx
@@ -1,42 +1,56 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import fcscLogo from '../../../public/logo/fcsc.png'
+import { withRouter } from 'next/router'
 
-export default function Navbar(): JSX.Element {
+interface NavbarProps {
+  router: any
+}
+
+function Navbar({ router }: NavbarProps): JSX.Element {
+  const navs = [
+    { text: 'Home', href: '/' },
+    { text: 'Community', href: '/community' },
+    { text: 'Event', href: '/events' },
+    { text: 'Notice', href: '/notices' },
+    { text: 'About', href: '/about' },
+  ]
+
   return (
-    <header className="text-white body-font bg-gradient-to-r from-fcsc-blue via-fcsc-orange to-fcsc-blue">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/>
-          </svg>
-          <span className="ml-3 text-xl">FCSC</span>
-        </a>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <Link href="/">
-            <a className="mr-5 hover:text-gray-900"> Home</a>
-          </Link>
-          <Link href="/">
-            <a className="mr-5 hover:text-gray-900">Community</a>
-          </Link>
-          <Link href="/events">
-            <a className="mr-5 hover:text-gray-900">Event</a>
-          </Link>
-          <Link href="/notices">
-            <a className="mr-5 hover:text-gray-900">Notice</a>
-          </Link>
-          <Link href="/">
-            <a className="mr-5 hover:text-gray-900">About</a>
-          </Link>
-        </nav>
+    <header className="sticky top-0 flex flex-row w-full h-14 justify-between items-center shadow-noOffset z-10">
+      <div className="w-28 h-12 flex flex-row items-center ml-9">
+        <Image src={fcscLogo} alt="FCSC Logo" className="w-max" />
+      </div>
+      <div className="flex flex-row justify-end  items-center w-4/6 h-14 bg-white">
+        <div className="flex flex-row mr-2">
+          {navs.map((nav, key) => (
+            <div
+              key={key}
+              className={`flex flex-row items-center justify-center mr-10 h-7 rounded-md bg-fcsc-orange text-center font-semibold ${
+                nav.text == 'Community' ? 'w-28' : ' w-16'
+              }`}
+            >
+              <Link href={nav.href}>
+                <a
+                  className={`${
+                    router.pathname == nav.href
+                      ? 'text-white w-28'
+                      : 'text-fcsc-blue'
+                  }`}
+                >
+                  {nav.text}
+                </a>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <label className="md:block sm :block lg:hidden" htmlFor="toggle">
+          &#9776;
+        </label>
+        <input className="hidden" type="checkbox" id="toggle" />
       </div>
     </header>
   )
 }
+
+export default withRouter(Navbar)

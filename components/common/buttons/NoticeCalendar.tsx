48c26227f912400dc41089d4b1fe689b2b17f70e
Akalanka Perera <akalankaperera128@gmail.com>
2021-09-03 21:03:21 +0530

    build error fix step - 2

---
+++ components/common/buttons/NoticeCalendar.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { MdPermContactCalendar } from 'react-icons/md'
+
+export default function NoticeCalendar(): JSX.Element {
+  useEffect(() => {
+    Aos.init({ offset: 0, duration: 1000 })
+  }, [])
+  
+  return (
+    <div
+      className="w-full md:w-11/12 flex flex-row justify-center md:justify-end"
+      data-aos="fade-left"
+    >
+      <div className="bg-fcsc-orange mr-0 md:mr-4 px-5 py-3 rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 text-white font-semibold cursor-pointer transition ease-in duration-200 flex flex-row items-center">
+        <MdPermContactCalendar size={28} className="mr-3" />
+        <div>Notice Calendar</div>
+      </div>
+    </div>
+  )
+}

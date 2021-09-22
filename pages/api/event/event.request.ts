c72613c13bd00673002d92acb0b6051b7572de04
Akalanka Perera <akalankaperera128@gmail.com>
2021-09-23 00:32:56 +0530

    Update/event-endpoint
    Synced with backend

--- pages/api/event/event.request.ts
+++ pages/api/event/event.request.ts
@@ -3,7 +3,7 @@ import { EventData } from './event.interface'
 
 export async function getEvents(): Promise<EventData[] | boolean> {
   try {
-    const response = await apiInstance.get('/events/fcsc')
+    const response = await apiInstance.get('/events/allevents/fcsc')
     return response.data as EventData[]
   } catch (e: any) {
     console.error(e.message)

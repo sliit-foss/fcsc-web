00f2b25e34b00fdd53cfe397d402fb63f5404b4b
Akalanka Perera <akalankaperera128@gmail.com>
2021-10-22 12:23:28 +0530

    Hotfix/mobile-responsiveness (#69)

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -6,7 +6,10 @@ export default function Layout({ children }: any): JSX.Element {
   return (
     <div>
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="viewport"
+          content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"
+        />
       </Head>
       <main className="overflow-x-hidden">
         <Navbar />

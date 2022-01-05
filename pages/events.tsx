a0699d778e7e8ed5e4439546cbd7c2b11359f1eb
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-01-05 22:02:12 +0530

    Event page "What are events" content #81 (#91)
    * feat(notice):what are notices
    
    * feat(events):what are events

--- pages/events.tsx
+++ pages/events.tsx
@@ -65,15 +65,19 @@ export default function Events(): JSX.Element {
         <title>Events</title>
       </Head>
       <div className="justify-items-center ">
-        <HeaderImage
-          position="absolute top-14"
-        />
-        <h1 className="title-font sm:text-4xl text-3xl font-medium text-white relative top-20 left-6 sm2:left-10 sm:left-12 z-20 flex md:hidden" data-aos="fade-right">
+        <HeaderImage position="absolute top-14" />
+        <h1
+          className="title-font sm:text-4xl text-3xl font-medium text-white relative top-20 left-6 sm2:left-10 sm:left-12 z-20 flex md:hidden"
+          data-aos="fade-right"
+        >
           EVENTS
         </h1>
         <div className="flex flex-col-reverse md:flex-row pb-0 md:pb-6 mt-44 md:mt-0 place-content-center items-center relative top-0 z-20">
           <div className="flex flex-col w-full md:w-1/2 place-content-center p-14 px-10 md:px-14">
-            <h1 className="title-font sm:text-4xl text-3xl font-medium text-white mt-24 xl:mt-40 mb-64 xl:mb-96 hidden md:flex" data-aos="fade-down">
+            <h1
+              className="title-font sm:text-4xl text-3xl font-medium text-white mt-24 xl:mt-40 mb-64 xl:mb-96 hidden md:flex"
+              data-aos="fade-down"
+            >
               EVENTS
             </h1>
             <h1
@@ -83,18 +87,26 @@ export default function Events(): JSX.Element {
               What Are Events
             </h1>
             <p className="mt-2 text-center md:text-left" data-aos="fade-right">
-              <span className="text-3xl font-semibold">L</span>orem ipsum dolor
-              sit amet, consectetur adipiscing elit, sed modtur adipiscing elit,
-              sed modo consequat. Duis nulla pariatur. Eident, sunt intur
-              adipiscing elit, sed modo consequat. Duis nulla pariatur. Eident,
-              sunt intur adipiscing elit, sed modo consequat. Duis nulla
-              pariatur. Eident, sunt ino consequat. Duis nulla pariatur. Eident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-              ipiscing elit, sed modtur adipiscing elit, sed modo consequat.
-              Duis nulla pariatur. Eident, sunt intur adipiscing elit, sed modo
-              consequat. Duis nulla pariatur. Eident, sunt intur adipiscing
-              elit, sed modo consequat. Duis nulla pariatur. Eident, sunt ino
-              consequat. Duis nulla pariatur. Eident
+              <span className="text-3xl font-semibold">S</span>ince its
+              inception, SLIIT has always been offering adequate importance to
+              both academics and extracurricular activities. FCSC being a prime
+              potent force within the university has contributed immensely in
+              realizing the aforesaid objective. Accordingly, throughout its
+              legacy, FCSC has brought an array of events into light forging a
+              firm platform for the student community to unlock their potentials
+              and unleash them effectively. <b>Virayama</b> the premier
+              classical musical extravaganza annually held in the university is
+              yet another event proudly presented by the FCSC in its calendar of
+              events that spans a wide range of events. Furthermore, in spite of
+              the difficulties caused by the global pandemic, FCSC resiliently
+              organized a variety of virtual events to enrich students with both
+              productive and positive experiences no less than before. The
+              ultimate motive behind them has always been to infuse an equal
+              blend and balance between studies and civic life of students and
+              help them groom into wholesome personalities by successfully
+              managing the friction of exams, peer pressure, deadlines, and
+              tight timelines. Thus over the years it is evident how they have
+              enabled improving both social and life skills of the students.
             </p>
           </div>
           <div className="w-3/4 md:w-4/12" data-aos="fade-left">

0505652e0e3393684f285f1d4e5ac429a4a0f36a
Akalanka Perera <akalanka@parallax.lk>
2022-01-07 20:10:24 +0530

    Added social links on member card hover (#92)

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import { TiArrowSortedDown } from 'react-icons/ti'
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
 import HeaderImage from '../components/common/HeaderImage'
 import image from '../public/aboutUs/aboutUs.svg'
 import photo from '../public/aboutUs/photo.png'
@@ -15,8 +16,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -26,8 +27,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -37,8 +38,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -48,8 +49,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -59,8 +60,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -70,8 +71,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -81,8 +82,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -92,9 +93,9 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
-  }
+      linkedln: 'linkedlnLink',
+    },
+  },
 ]
 export default function AboutUs(): JSX.Element {
   const currentTime = new Date()
@@ -118,20 +119,19 @@ export default function AboutUs(): JSX.Element {
         <title>About</title>
       </Head>
       <div className="justify-items-center">
-        <HeaderImage
-          position="absolute top-14"
-        />
+        <HeaderImage position="absolute top-14" />
         <h1
           className="title-font sm:text-4xl text-3xl font-medium text-white relative top-20 left-6 sm2:left-10 sm:left-12 z-20 flex md:hidden"
-          data-aos="fade-right">
+          data-aos="fade-right"
+        >
           ABOUT US
         </h1>
-        <div
-          className="flex flex-col-reverse md:flex-row pb-0 md:pb-6 mt-44 md:mt-0 place-content-center items-center relative top-0 z-20">
+        <div className="flex flex-col-reverse md:flex-row pb-0 md:pb-6 mt-44 md:mt-0 place-content-center items-center relative top-0 z-20">
           <div className="flex flex-col w-full md:w-1/2 place-content-center p-14 px-10 md:px-14">
             <h1
               className="title-font sm:text-4xl text-3xl font-medium text-white mt-24 xl:mt-40 mb-64 xl:mb-96 hidden md:flex"
-              data-aos="fade-down">
+              data-aos="fade-down"
+            >
               ABOUT US
             </h1>
             <h1
@@ -140,17 +140,28 @@ export default function AboutUs(): JSX.Element {
             >
               Who We Are
             </h1>
-            <p className="mt-2 text-center md:text-left lg:text-justify" data-aos="fade-right">
-              <span className="text-3xl font-semibold">T</span>he Faculty of Computing Student Community also known as
-              the FCSC is the largest student body of the Faculty of Computing. The Faculty of Computing Student
-              Community was established under the Faculty of Computing and consists of Computing undergraduates.
+            <p
+              className="mt-2 text-center md:text-left lg:text-justify"
+              data-aos="fade-right"
+            >
+              <span className="text-3xl font-semibold">T</span>he Faculty of
+              Computing Student Community also known as the FCSC is the largest
+              student body of the Faculty of Computing. The Faculty of Computing
+              Student Community was established under the Faculty of Computing
+              and consists of Computing undergraduates.
             </p>
-            <p className="mt-2 text-center md:text-left lg:text-justify" data-aos="fade-right">
-              Since there is a growing competition in the Sri Lankan job market in relation to this field of knowledge,
-              we focus on nurturing the hard and soft skills which would improve their employability after the
-              successful completion of the degree, as well as organizing events for the Student Community to grow
-              academically as well as to get in touch with their creative senses, to promote a stress free and memorable
-              University experience. The following are some of the main events organized by our Student Community,
+            <p
+              className="mt-2 text-center md:text-left lg:text-justify"
+              data-aos="fade-right"
+            >
+              Since there is a growing competition in the Sri Lankan job market
+              in relation to this field of knowledge, we focus on nurturing the
+              hard and soft skills which would improve their employability after
+              the successful completion of the degree, as well as organizing
+              events for the Student Community to grow academically as well as
+              to get in touch with their creative senses, to promote a stress
+              free and memorable University experience. The following are some
+              of the main events organized by our Student Community,
             </p>
           </div>
           <div className="w-3/4 md:w-4/12" data-aos="fade-left">
@@ -192,7 +203,7 @@ export default function AboutUs(): JSX.Element {
           {board.map((member) => (
             <div
               key={member.socialLinks.fb}
-              className="m-10 w-full md:w-4/12 lg:w-1/4 aspect-w-1.25 aspect-h-1 md:aspect-w-3 md:aspect-h-1 lg:aspect-w-4 lg:aspect-h-1 relative rounded-3xl shadow-md flex justify-center items-center"
+              className="relative group m-10 w-full md:w-4/12 lg:w-1/4 aspect-w-1.25 aspect-h-1 md:aspect-w-3 md:aspect-h-1 lg:aspect-w-4 lg:aspect-h-1 relative rounded-3xl shadow-md flex justify-center items-center"
               data-aos="fade-up"
             >
               <Image
@@ -204,6 +215,39 @@ export default function AboutUs(): JSX.Element {
                 objectFit="cover"
                 placeholder="blur"
               />
+              <div className="absolute bottom-0 h-0 w-full group-hover:h-full transition-all ease-out duration-500 flex flex-col items-center justify-center bg-gradient-to-t from-fcsc-orange to-fcsc-orange_light rounded-xl md:group-hover:p-4">
+                <h2 className="text-base sm:text-2xl md:text-3xl font-bold text-white group-hover:text-opacity-100 text-opacity-0 text-center mb-4">
+                  <div
+                    className="sm:mb-3 md:mb-3 flex flex-row z-0"
+                    data-aos="fade-down"
+                  >
+                    <a href={member.socialLinks.fb}>
+                      <FaFacebook
+                        className="mb-3 mr-6 fill-current-color text-white hover:text-gray-600 transition ease-in duration-200"
+                        size="30"
+                      />
+                    </a>
+                    <a href={member.socialLinks.insta}>
+                      <FaInstagram
+                        className="mb-3 mr-6 fill-current-color text-white hover:text-gray-600 transition ease-in duration-200"
+                        size="30"
+                      />
+                    </a>
+                    <a href={member.socialLinks.linkedln}>
+                      <FaLinkedin
+                        className="mb-3 mr-6 fill-current-color text-white hover:text-gray-600 transition ease-in duration-200"
+                        size="30"
+                      />
+                    </a>
+                    <a href={member.socialLinks.twitter}>
+                      <FaTwitter
+                        className="mb-3 mr-3 fill-current-color text-white hover:text-gray-600 transition ease-in duration-200"
+                        size="30"
+                      />
+                    </a>
+                  </div>
+                </h2>
+              </div>
             </div>
           ))}
         </div>

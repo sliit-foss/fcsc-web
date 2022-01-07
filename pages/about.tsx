4fb26c8b76b742e0250f0c0d65779cd4c25bec7a
MadhushaPrasad <madushaprasad21@gmail.com>
2022-01-07 20:12:03 +0530

    about us

--- pages/about.tsx
+++ pages/about.tsx
@@ -15,8 +15,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -26,8 +26,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -37,8 +37,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -48,8 +48,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -59,8 +59,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -70,8 +70,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -81,8 +81,8 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
+      linkedln: 'linkedlnLink',
+    },
   },
   {
     name: 'name',
@@ -92,9 +92,9 @@ const board = [
       fb: 'fbLink',
       twitter: 'twitterLink',
       insta: 'instaLink',
-      linkedln: 'linkedlnLink'
-    }
-  }
+      linkedln: 'linkedlnLink',
+    },
+  },
 ]
 export default function AboutUs(): JSX.Element {
   const currentTime = new Date()
@@ -118,20 +118,19 @@ export default function AboutUs(): JSX.Element {
         <title>About</title>
       </Head>
       <div className="justify-items-center">
-        <HeaderImage
-          position="absolute top-14"
-        />
+        <HeaderImage position="absolute top-14" />
         <h1
           className="title-font sm:text-4xl text-3xl font-medium text-white relative top-20 left-6 sm2:left-10 sm:left-12 z-20 flex md:hidden"
-          data-aos="fade-right">
+          data-aos="fade-right"
+        >
           ABOUT US
         </h1>
-        <div
-          className="flex flex-col-reverse md:flex-row pb-0 md:pb-6 mt-44 md:mt-0 place-content-center items-center relative top-0 z-20">
+        <div className="flex flex-col-reverse md:flex-row pb-0 md:pb-6 mt-44 md:mt-0 place-content-center items-center relative top-0 z-20">
           <div className="flex flex-col w-full md:w-1/2 place-content-center p-14 px-10 md:px-14">
             <h1
               className="title-font sm:text-4xl text-3xl font-medium text-white mt-24 xl:mt-40 mb-64 xl:mb-96 hidden md:flex"
-              data-aos="fade-down">
+              data-aos="fade-down"
+            >
               ABOUT US
             </h1>
             <h1
@@ -140,17 +139,28 @@ export default function AboutUs(): JSX.Element {
             >
               Who We Are
             </h1>
-            <p className="mt-2 text-center md:text-left lg:text-justify" data-aos="fade-right">
-              <span className="text-3xl font-semibold">T</span>he Faculty of Computing Student Community also known as
-              the FCSC is the largest student body of the Faculty of Computing. The Faculty of Computing Student
-              Community was established under the Faculty of Computing and consists of Computing undergraduates.
+            <p
+              className="mt-2 text-center md:text-left lg:text-justify"
+              data-aos="fade-right"
+            >
+              <span className="text-3xl font-semibold">T</span>he Faculty of
+              Computing Student Community also known as the FCSC is the largest
+              student body of the Faculty of Computing. The Faculty of Computing
+              Student Community was established under the Faculty of Computing
+              and consists of Computing undergraduates.
             </p>
-            <p className="mt-2 text-center md:text-left lg:text-justify" data-aos="fade-right">
-              Since there is a growing competition in the Sri Lankan job market in relation to this field of knowledge,
-              we focus on nurturing the hard and soft skills which would improve their employability after the
-              successful completion of the degree, as well as organizing events for the Student Community to grow
-              academically as well as to get in touch with their creative senses, to promote a stress free and memorable
-              University experience. The following are some of the main events organized by our Student Community,
+            <p
+              className="mt-2 text-center md:text-left lg:text-justify"
+              data-aos="fade-right"
+            >
+              Since there is a growing competition in the Sri Lankan job market
+              in relation to this field of knowledge, we focus on nurturing the
+              hard and soft skills which would improve their employability after
+              the successful completion of the degree, as well as organizing
+              events for the Student Community to grow academically as well as
+              to get in touch with their creative senses, to promote a stress
+              free and memorable University experience. The following are some
+              of the main events organized by our Student Community,
             </p>
           </div>
           <div className="w-3/4 md:w-4/12" data-aos="fade-left">

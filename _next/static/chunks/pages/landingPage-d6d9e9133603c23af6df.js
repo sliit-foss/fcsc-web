4f3a6911a40894702feb25651a2daf47e18c98a2
Akalanka47000 <akalankaperera128@gmail.com>
2023-08-02 18:28:49 +0000

    Deploying to gh-pages from @ sliit-foss/fcsc-web@b649e16a40cbd92052434cbd867b9e602166f0fe 🚀

---
+++ _next/static/chunks/pages/landingPage-d6d9e9133603c23af6df.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[653],{3765:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/landingPage",function(){return u(9138)}])}},function(n){n.O(0,[774,788,138,888,179],(function(){return _=3765,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
